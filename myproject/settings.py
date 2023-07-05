ccb71ff39a20747a8e7b464cf08d833174ee97d9
kapyanammu1 <xtianized1@gmail.com>
2023-07-06 04:30:17 +0800

    dsds

--- myproject/settings.py
+++ myproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2evi-@16kiypyci=+)d&f=57b33wtr0+q38rkj(^cxh1mcolpg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
